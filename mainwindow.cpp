d8acd8f9c7bdeb1a3c8cad97e2058451c576b699
Laura Ekstrand <laura@jlekstrand.net>
2017-11-06 19:55:01 -0800

    Rendering from local HTML with Reveal.js.  Needs qt5-qtwebengine-freeworld.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,22 @@
 #include "mainwindow.h"
+#include <QFile>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
   view = new QWebEngineView(this);
   setCentralWidget(view);
+
+  QFile file("../helloworld.html");
+  if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    return;
+
+  QTextStream in (&file);
+  html = in.readAll();
+  view->setHtml(html, QUrl::fromLocalFile("/home/laura/programming/life-verse/"));
+  // view->load(QUrl("http://localhost:3000/helloworld.html"));
+  view->show();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ public:
 
 private:
     QWebEngineView* view;
+    QString html;
 };
 
 #endif // MAINWINDOW_H
